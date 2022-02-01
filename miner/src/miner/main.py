208fc6eed1db9c1bb0ae673ec3810f4b7bfa92e3
Christian Birchler <christian.birchler2@uzh.ch>
2022-02-01 23:42:12 +0100

    List all python projects desc. order of stargazers

--- miner/src/miner/main.py
+++ miner/src/miner/main.py
@@ -1,3 +1,10 @@
+from github import Github
 
 if __name__ == '__main__':
     print('* start miner ...')
+
+    g = Github('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')
+
+    repositories = g.search_repositories(query='language:Python', sort='stars', order='desc')
+    for repo in repositories:
+        print('{} stars: {}'.format(repo.name, repo.stargazers_count))
