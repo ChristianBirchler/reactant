ae480475d9006b6639268d1dbca06e04614c6c85
Christian Birchler <christian.birchler2@uzh.ch>
2022-02-10 20:31:30 +0100

    Run Plotly in a separate process

--- visualizer/src/visualizer/main.py
+++ visualizer/src/visualizer/main.py
@@ -1,21 +1,21 @@
 from multiprocessing.managers import BaseManager
 from multiprocessing import Process, Queue
+from plotly_dash import run_plotly_dash_app
 
 
 class QueueManager(BaseManager):
     pass
 
 
-class Worker(Process):
-    def __init__(self, queue_):
+class PlotlyProcess(Process):
+    def __init__(self, msg_queue_python, msg_queue_java):
         super().__init__()
-        self.queue = queue_
+        self.msg_queue_python = msg_queue_python
+        self.msg_queue_java = msg_queue_java
 
     def run(self):
-        while True:
-            print('while loop')
-            element = self.queue.get()
-            print(element)
+        print('* plotly process started')
+        run_plotly_dash_app(msg_queue_python=self.msg_queue_python, msg_queue_java=self.msg_queue_java, debug=False)
 
 
 class QueueManagerProcess(Process):
@@ -23,16 +23,25 @@ class QueueManagerProcess(Process):
         super().__init__()
 
     def run(self):
-        queue = Queue()
+        print('* queue manager process started')
 
-        worker = Worker(queue)
-        worker.start()
-        print('* worker started')
+        python_queue = Queue()
+        java_queue = Queue()
 
-        QueueManager.register('get_queue', callable=lambda: queue)
-        m = QueueManager(address=('', 50000), authkey=b'abracadabra')
-        s = m.get_server()
-        s.serve_forever()
+        plotly_process = PlotlyProcess(python_queue, java_queue)
+        plotly_process.start()
+
+        QueueManager.register('get_python_queue', callable=lambda: python_queue)
+        QueueManager.register('get_java_queue', callable=lambda: java_queue)
+
+        queue_manager = QueueManager(address=('', 50000), authkey=b'abracadabra')
+        server = queue_manager.get_server()
+
+        print('* start queue manager server')
+        server.serve_forever()
+        print('* queue manager server started')
+        while True:
+            pass
 
 
 if __name__ == '__main__':
